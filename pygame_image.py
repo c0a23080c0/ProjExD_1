9a45b9e491262410554f7ad7a7dc3276228899e0
shimizu.EX <c0a23080c0@edu.teu.ac.jp>
2024-06-11 16:55:07 +0900

    こうかとん動く

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,8 @@ def main():
     kouk_img = pg.image.load("fig/3.png")
     kouk_img = pg.transform.flip(kouk_img,True,False)    
     tmr = 0
+    kouk_rct = kouk_img.get_rect()
+    kouk_rct.center =  300 , 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -23,8 +25,17 @@ def main():
         screen.blit(se_bg_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(se_bg_img, [-x+4800, 0])
-        kouk_rct = kouk_img.get_rect()
-        kouk_rct.center =  300 , 200
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kouk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kouk_rct.move_ip(0, +1)
+        if key_lst[pg.K_RIGHT]:
+            kouk_rct.move_ip(+1, 0)
+        if key_lst[pg.K_LEFT]:
+            kouk_rct.move_ip(-1, 0)
+
         screen.blit(kouk_img,kouk_rct)
         pg.display.update()
         tmr += 1        
